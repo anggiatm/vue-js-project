b0ccca6991cf1637f66eab41eb0239623689489b
anggiatm <anggiat17@gmail.com>
2022-02-17 14:49:13 +0700

    first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <h2>Best Project</h2>
+    </div>
+    <div class="col">
+      <img src="../assets/portfolio_main.svg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style></style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="projects">
+    <Navbar />
+    <h2>Daftar Projects</h2>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "Projects",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style></style>
